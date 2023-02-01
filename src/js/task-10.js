9dba5ee6c9ad3fdb48e653c013a0031becdffa17
Vladyslav Simak <simakv21@gmail.com>
2023-02-01 22:08:02 +0200

    Version 6

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  amount = input.value;
+  for (let i = 0; i < input.value; i+=1) {
+    const box = document.createElement('div');
+    box.style.width = `${30 + 10 * i}px`;
+    box.style.height = `${30 + 10 * i}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.append(box);
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+};
