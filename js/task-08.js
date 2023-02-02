bedd5c49e81ea8948cf46fdbda84658dc50e766b
Vladyslav Simak <simakv21@gmail.com>
2023-02-02 09:11:24 +0200

    Update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('.login-form');
 
-form.addEventListener('click', onFormSubmit);
+form.addEventListener('submit', onFormSubmit);
 
 
 function onFormSubmit(event) {
