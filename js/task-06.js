3edd932b7bd1a534432ef6002fa44bcf07bc197d
Vladyslav Simak <simakv21@gmail.com>
2023-02-02 16:21:39 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,6 @@ const input = document.querySelector('input');
 let dataLength = input.getAttribute('data-length');
 
 input.addEventListener("blur", (event) => {
-  console.log(event.target);
   if(event.target.value.length === Number(dataLength)) {
     event.target.classList.add('valid');
     event.target.classList.remove('invalid');
